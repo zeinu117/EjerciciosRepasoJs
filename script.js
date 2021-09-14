e37696e05477ff9f32c1e73a685128a045dded4c
zeinu117 <cesar_zein@hotmail.com>
2021-09-14 14:01:40 -0500

    version 1

---
+++ script.js
@@ -0,0 +1,22 @@
+//1
+/* 
+    for (let index = 0; index < 10; index++) {
+        console.log(index);
+    }
+*/
+
+//2
+/* 
+    for (let i = 50; i < 151; i++) {
+        console.log(i);
+    }
+*/
+
+//3
+/* 
+    for (let i = 8; i < 41; i =i+2) {
+        
+        console.log(i);
+    }
+*/
+
